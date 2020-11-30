8dfe7af739d51c54c2fa78bdcd469c10df366633
Linus <Linus@LINUS-PC>
2020-11-30 17:10:05 +0100

    add boards

--- src/sudoku/sudokuTest.java
+++ src/sudoku/sudokuTest.java
@@ -13,16 +13,32 @@ public class sudokuTest {
 
     private int[][] board1;
     private int[][] board2;
-    private Sudoku sudoko1;
-    private Sudoku sudoko2;
+    private Sudoku sudoku1;
 
 
     @Before
     public void setUp() throws Exception {
-        board1 = new int[9][9];
-        board2 = new int[9][9];
-        sudoko1 = new Sudoku(board1);
-        sudoko2 = new Sudoku(board2);   
+        board1 = new int[][] { { 2, 7, 4, 0, 9, 0, 6, 5, 3 },
+        { 3, 9, 6, 5, 7, 4, 8, 0, 0 },
+        { 0, 4, 0, 6, 1, 8, 3, 9, 7 },
+        { 7, 6, 1, 0, 4, 0, 5, 2, 8 },
+        { 9, 3, 8, 7, 2, 5, 0, 6, 3 },
+        { 1, 0, 0, 4, 5, 6, 7, 8, 9 },
+        { 4, 5, 7, 0, 8, 0, 2, 3, 6 },
+        { 6, 8, 9, 2, 3, 7, 0, 4, 0 },
+        { 0, 0, 5, 3, 6, 2, 9, 7, 4 }
+        };
+        board2 = new int[][] { { 2, 7, 4, 0, 9, 0, 6, 5, 0 },
+        { 3, 9, 6, 5, 7, 4, 8, 0, 0 },
+        { 0, 4, 0, 6, 1, 8, 3, 9, 7 },
+        { 7, 6, 1, 0, 4, 0, 5, 2, 8 },
+        { 9, 3, 8, 7, 2, 5, 0, 6, 3 },
+        { 1, 0, 0, 4, 5, 6, 7, 8, 9 },
+        { 4, 5, 7, 0, 8, 0, 2, 3, 6 },
+        { 6, 8, 9, 2, 3, 7, 0, 4, 0 },
+        { 0, 0, 5, 3, 6, 2, 9, 7, 4 }
+        };
+        sudoku1 = new Sudoku(board1);   
     }
 
     @After
@@ -33,16 +49,7 @@ public class sudokuTest {
 
     @Test
     public void testImpSudoku(){
-        board1 = new int[][] { { 2, 7, 4, 0, 9, 0, 6, 5, 3 },
-                                { 3, 9, 6, 5, 7, 4, 8, 0, 0 },
-                                { 0, 4, 0, 6, 1, 8, 3, 9, 7 },
-                                { 7, 6, 1, 0, 4, 0, 5, 2, 8 },
-                                { 9, 3, 8, 7, 2, 5, 0, 6, 3 },
-                                { 1, 0, 0, 4, 5, 6, 7, 8, 9 },
-								{ 4, 5, 7, 0, 8, 0, 2, 3, 6 },
-								{ 6, 8, 9, 2, 3, 7, 0, 4, 0 },
-								{ 0, 0, 5, 3, 6, 2, 9, 7, 4 }
-								};
+        
     }
     
     @Test
