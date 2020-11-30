d783c9a4f304554122ceb9f3b0fdaecab5e15955
Jacob Rinderud <jacob.a.rinderud@gmail.com>
2020-11-30 18:45:51 +0100

    Börjat på grafiken

---
+++ src/sudoku/Graphical.java
@@ -0,0 +1,38 @@
+package sudoku;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+public class Graphical {
+
+    public Graphical(int bigness) {
+        SwingUtilities.invokeLater(() -> createWindow(bigness));
+    }
+
+    private void createWindow(int n) {
+        JFrame frame = new JFrame("Solving sodukus since 1999");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container windowPane = frame.getContentPane();
+        windowPane.setLayout(new GridLayout(n, n));
+
+        JPanel[] panelMatrix = new JPanel[n*n];
+
+        for (int i = 0; i < panelMatrix.length; i++){
+            panelMatrix[i] = new JPanel();
+            panelMatrix[i].setLayout(new GridLayout(n, n));
+            for(int j = 0; j < n*n; j++){
+                panelMatrix[i].add(new JLabel(i + ":" + j));
+            }
+            panelMatrix[i].setBackground(new Color(i*5, i*5, i*5));
+            windowPane.add(panelMatrix[i]);
+        }
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Graphical(3);
+    }
+}
