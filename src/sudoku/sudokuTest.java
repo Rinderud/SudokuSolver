e8b240e0c8abdc75ac87443c2df623e3425051b2
Jacob Rinderud <jacob.a.rinderud@gmail.com>
2020-11-30 13:09:46 +0100

    Skapade testfilen. Men JUnit är inte i projektet än.

---
+++ src/sudoku/sudokuTest.java
@@ -0,0 +1,72 @@
+package sudoku;
+
+//import static org.junit.jupiter.api.Assertions.*;
+
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+
+class sudokuTest {
+    
+
+    @BeforeEach
+    void setUp() throws Exception{
+
+    }
+
+    @AfterEach
+    void tearDown() throws Exception{
+
+    }
+
+    @Test
+    void testSudoku(){
+        
+    }
+
+    @Test
+    void testSolve(){
+        
+    }
+
+    @Test
+    void testCheckRules(){
+        
+    }
+
+    @Test
+    void testClear(){
+        
+    }
+
+    @Test
+    void testSetNumber(){
+        
+    }
+
+    @Test
+    void testTrySetNumber(){
+        
+    }
+
+    @Test
+    void testGetNumber(){
+        
+    }
+
+    @Test
+    void testRemoveNumber(){
+        
+    }
+
+    @Test
+    void testGetNumbers(){
+        
+    }
+
+    @Test
+    void testSetNumbers(){
+        
+    }
+
+}
