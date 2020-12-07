e076295712912f4c7ae92dd55a488ba705ebec4a
Jacob Rinderud <jacob.a.rinderud@gmail.com>
2020-12-07 14:11:48 +0100

    Ändrade till textField

--- src/sudoku/Graphical.java
+++ src/sudoku/Graphical.java
@@ -6,19 +6,19 @@ import java.awt.Font;
 import java.awt.GridLayout;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
 public class Graphical {
-    private final static int[][] board = new int[][] {
-        { 2, 7, 4, 0, 9, 0, 6, 5, 3 },
+    private final static int[][] board = new int[][] { 
+        { 2, 7, 4, 0, 9, 0, 6, 5, 3 }, 
         { 3, 9, 6, 5, 7, 4, 8, 0, 0 },
-        { 0, 4, 0, 6, 1, 8, 3, 9, 7 },
-        { 7, 6, 1, 0, 4, 0, 5, 2, 8 },
+        { 0, 4, 0, 6, 1, 8, 3, 9, 7 }, 
+        { 7, 6, 1, 0, 4, 0, 5, 2, 8 }, 
         { 9, 3, 8, 7, 2, 5, 0, 6, 3 },
-        { 1, 0, 0, 4, 5, 6, 7, 8, 9 },
-        { 4, 5, 7, 0, 8, 0, 2, 3, 6 },
+        { 1, 0, 0, 4, 5, 6, 7, 8, 9 }, 
+        { 4, 5, 7, 0, 8, 0, 2, 3, 6 }, 
         { 6, 8, 9, 2, 3, 7, 0, 4, 0 },
         { 0, 0, 5, 3, 6, 2, 9, 7, 4 } };
 
@@ -34,32 +34,29 @@ public class Graphical {
 
         JPanel[] panelMatrix = new JPanel[n * n];
 
-        
-
-
-/**
- * Loopen behöver göras om:
- * Den ska loopa logiskt, dvs. som man loopar igenom en 2d matris.
- * Den grafiska delen behöver då skrivas om,
- * samma delar men kanske i en annan loop och sparas annorlunda.
- */
-        for (int i = 0; i < panelMatrix.length; i++){
+        /**
+         * Loopen behöver göras om: Den ska loopa logiskt, dvs. som man loopar igenom en
+         * 2d matris. Den grafiska delen behöver då skrivas om, samma delar men kanske i
+         * en annan loop och sparas annorlunda.
+         */
+        for (int i = 0; i < panelMatrix.length; i++) {
             panelMatrix[i] = new JPanel();
             panelMatrix[i].setLayout(new GridLayout(n, n));
-            if (i % 2 == 1) {
-                panelMatrix[i].setBackground(new Color(150, 150, 150));     // Färgar hälften av 3x3 rutorna
+        }
+
+        for (int x = 0; x < 9; x++) {
+            for (int y = 0; y < 9; y++) {
+                JTextField textField = new JTextField("  " + sudoku.getNumber(x, y) + "  ");
+                textField.setFont(new Font("Serif", Font.BOLD, 100)); // Större text
+                textField.setHorizontalAlignment(textField.CENTER);
+                if (conversion(x, y) % 2 == 0) {
+                    textField.setBackground(new Color(254, 127, 73));
+                }
+                panelMatrix[conversion(x, y)].add(textField);
             }
         }
 
-        for (int x = 0; x < 9; x++){
-            for (int y = 0; y < 9; y++){
-                JLabel label = new JLabel("  " + sudoku.getNumber(x, y) + "  ");
-                label.setFont(new Font("Serif", Font.BOLD, 100));       // Större text
-                panelMatrix[conversion(x, y)].add(label);
-            }
-        }
-
-        for (int i = 0; i < panelMatrix.length; i++){
+        for (int i = 0; i < panelMatrix.length; i++) {
             windowPane.add(panelMatrix[i]);
         }
 
@@ -74,28 +71,28 @@ public class Graphical {
      * @return The index of the pane that number should be in
      */
     private int conversion(int x, int y) {
-        if (x < 3 && 0 <= x){
-            if (y < 3 && 0 <= y){
+        if (x < 3 && 0 <= x) {
+            if (y < 3 && 0 <= y) {
                 return 0;
-            }else if (y < 6 && 3 <= y){
+            } else if (y < 6 && 3 <= y) {
                 return 1;
-            }else{
+            } else {
                 return 2;
             }
-        }else if (x < 6 && 3 <= x){
-            if (y < 3 && 0 <= y){
+        } else if (x < 6 && 3 <= x) {
+            if (y < 3 && 0 <= y) {
                 return 3;
-            }else if (y < 6 && 3 <= y){
+            } else if (y < 6 && 3 <= y) {
                 return 4;
-            }else{
+            } else {
                 return 5;
             }
-        }else{
-            if (y < 3 && 0 <= y){
+        } else {
+            if (y < 3 && 0 <= y) {
                 return 6;
-            }else if (y < 6 && 3 <= 6){
+            } else if (y < 6 && 3 <= 6) {
                 return 7;
-            }else{
+            } else {
                 return 8;
             }
         }
