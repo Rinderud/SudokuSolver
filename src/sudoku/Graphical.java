c15ec23dec25308f66ee76a36f8d285374de5faf
Jacob Rinderud <jacob.a.rinderud@gmail.com>
2020-12-11 10:59:36 +0100

    Tog bort test kod

--- src/sudoku/Graphical.java
+++ src/sudoku/Graphical.java
@@ -21,16 +21,6 @@ import javax.swing.SwingUtilities;
  * @author Jacob Rinderud & Linus Carlsson Creates a graphical gui
  */
 public class Graphical {
-    private final static int[][] board = new int[][] { { 2, 7, 4, 0, 9, 0, 6, 5, 3 }, { 3, 9, 6, 5, 7, 4, 8, 0, 0 },
-            { 0, 4, 0, 6, 1, 8, 3, 9, 7 }, { 7, 6, 1, 0, 4, 0, 5, 2, 8 }, { 9, 3, 8, 7, 2, 5, 0, 6, 3 },
-            { 1, 0, 0, 4, 5, 6, 7, 8, 9 }, { 4, 5, 7, 0, 8, 0, 2, 3, 6 }, { 6, 8, 9, 2, 3, 7, 0, 4, 0 },
-            { 0, 0, 5, 3, 6, 2, 9, 7, 4 } };
-
-    private final static int[][] boardSolvable = new int[][] { { 1, 0, 0, 2, 0, 0, 0, 0, 0 },
-            { 0, 2, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 3, 0, 4, 0, 0, 0, 0 }, { 0, 0, 4, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 7, 8, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0 } };
-
     public Graphical(Sudoku sudoku) {
         SwingUtilities.invokeLater(() -> createWindow(sudoku));
     }
@@ -190,7 +180,6 @@ public class Graphical {
 
     public static void main(String[] args) {
         Sudoku solveThis = new Sudoku();
-        solveThis.setNumbers(boardSolvable);
         new Graphical(solveThis);
     }
 }
