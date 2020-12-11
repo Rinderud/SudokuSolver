f2f0d07f29e7cd84599c2e42222e3b0b14523252
Li1633Ca <56188366+Li1633Ca@users.noreply.github.com>
2020-12-11 10:52:47 +0100

    måsvingar

--- src/sudoku/Sudoku.java
+++ src/sudoku/Sudoku.java
@@ -135,8 +135,9 @@ public class Sudoku implements SudokuSolver {
      */
     public void displayBoard() {
         for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++)
+            for (int j = 0; j < 9; j++) {
                 System.out.print(board[i][j] + " ");
+            }
             System.out.println();
         }
     }
