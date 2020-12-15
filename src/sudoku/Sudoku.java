232f226c2264810a889ac428b030be609b302b7a
Jacob Rinderud <jacob.a.rinderud@gmail.com>
2020-12-15 10:35:16 +0100

    tog bort sysout

--- src/sudoku/Sudoku.java
+++ src/sudoku/Sudoku.java
@@ -203,7 +203,6 @@ public class Sudoku implements SudokuSolver {
             }
             return false;
         } else {
-            System.out.println("illegal board");
             return false;
         }
 
